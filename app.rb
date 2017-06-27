0dea77ec257c128806fec33c06f6312da745ecb2
cheungth25 <cheungth25@gmail.com>
2017-06-27 14:49:44 -0400

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,31 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+  get '/reversename/:name' do
+    params[:name].reverse
+  end
+
+  get '/square/:number' do
+    square_ans = params[:number].to_i ** 2
+    square_ans.to_s
+  end
+
+  get '/say/:number/:phrase' do
+    @number = params[:number].to_i
+    @phrase = params[:phrase]
+    erb :say_phrase
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    word_array = [params[:word1], params[:word2], params[:word3], params[:word4], params[:word5]]
+    word_array.join(" ") + "."
+  end
+
+  get '/:operation/:number1/:number2' do
+    @operation = params[:operation]
+    @num1 = params[:number1].to_i
+    @num2 = params[:number2].to_i
+    erb :do_operation
+  end
 
 end
